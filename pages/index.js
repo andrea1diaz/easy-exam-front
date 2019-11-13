7c7e866f6e147b702ff8e61fcd2d83017dd0372e
dgcnz <canezdiego@gmail.com>
2019-11-13 08:29:28 -0500

    index

--- pages/index.js
+++ pages/index.js
@@ -6,116 +6,23 @@ import { Subscribe } from 'unstated';
 
 import AuthContainer from '../containers/authContainer'
 import LogIn from './LogIn';
-import SearchBar from '../components/molecules/SearchBar';
 
 class Index extends Component {
-		static async getInitialProps({ pathname }) {
-    const dataError = '';
-
-    try {
-      return {
-				pathname,
-        dataError: '',
-      };
-    } catch (e) {
-      console.log(e);
-      // dataError = e.response.data.message;
-      return {
-        pathname,
-        dataError,
-      };
-    }
-  }
-
-	constructor(props) {
-		super(props);
-		this.state = {
-			activeShare: false,
-			shareDetails: {},
-			loginOpened: true,
-			activePop: false,
-			loading: true,
-			loadingPopUpLogin: false,
-		};
-	}
-
-	componentDidMount() {
-    this.loading();
-  }
-
-  loading = async () => {
-    this.setState(s => {
-      return {
-        ...s,
-        loading: false,
-      };
-    });
-  };
-
-	render() {
+	render () {
 		return (
-			<Subscribe to={[AuthContainer]}>
-			{authState => {
-          this.authState = authState;
-          return (
-            <MediaQuery maxWidth={720}>
-              {isMobile => {
-                return (
-                  <div>
-                    <LogIn
-                      isMobile={isMobile}
-                      pathname={this.props.pathname}
-                      loading={this.state.loadingPopUpLogin}
-                      opened={this.state.loginOpened}
-                      onCancel={() => {
-                        this.setState(s => {
-                          return { ...s, loginOpened: false, activePop: false };
-                        });
-                      }}
-                      onLoginSuccess={async () => {
-                        this.setState(s => {
-                          return { ...s, loadingPopUpLogin: true };
-                        });
-                        this.setState(s => {
-                          return {
-                            ...s,
-                            loginOpened: false,
-                            activePop: false,
-                            loadingPopUpLogin: false
-                          };
-                        });
-                      }}
-                    />
-                    <Main activePop={this.state.activePop}>
-                      {this.state.loading ? (
-                        <LoadingBasicLayout />
-                      ) : (
-                        <BasicLayout
-                          isMobile={isMobile}
-                          withBack={false}
-                          pathname={this.props.pathname}
-                          title="Home"
-                          userData={authState.getUserData()}
-                          onLogout={() => {
-                            this.setState(s => {
-                              return {
-                                ...s,
-                              };
-                            });
-                          }}
-                        >
-                        </BasicLayout>
-                      )}
-                    </Main>
-                  </div>
-                );
-              }}
-            </MediaQuery>
-          );
-        }}
+			/* <Subscribe to={[AuthContainer]}>
+				{authState => {
+					this.authState = authState;
+					return (
+						<div>
+						</div>
+					)
+				}
+				}
+				<LogIn />
 			</Subscribe>
 			*/
-			<SearchBar />
+			<LogIn />
 		)
 	}
 }
